45585ea462760c696d15e49598636f09c78afdb6
Krzysztof Klekot <krzysiek.k366@gmail.com>
2018-11-06 18:11:31 +0100

    Add project to manage track. Create class Playlist and Track

---
+++ Eventovnia.Admin.Common/Playlist.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventovnia.Admin.Common
+{
+    public class Playlist
+    {
+        public string Title { get; private set; }
+        public TimeSpan PlaylistLength { get; private set; }
+
+        public List<Track> Tracks
+        {
+            get => Tracks;
+            set => PlaylistLength = CountLengthOfPlaylist();
+        }
+
+        public Playlist()
+        {
+            Title = "New playlist";
+            Tracks = new List<Track>();
+        }
+
+        public Playlist(string title)
+        {
+            Title = title;
+            Tracks = new List<Track>();
+        }
+
+        public Playlist(string title, params Track[] tracks)
+        {
+            Title = title;
+            Tracks = new List<Track>(tracks);
+        }
+
+        private TimeSpan CountLengthOfPlaylist()
+        {
+            int seconds = Tracks.Sum(x => x.Length.Seconds);
+            int minutes = Tracks.Sum(x => x.Length.Minutes);
+            int hours = Tracks.Sum(x => x.Length.Hours);
+            return new TimeSpan(hours, minutes, seconds);
+        }
+
+        public void AddTracks(params Track[] tracks)
+        {
+            Tracks.AddRange(tracks);
+        }
+
+        public void ReplaceTrack(Track track, int newPosition)
+        {
+            Tracks.Remove(track);
+            Tracks.Insert(newPosition, track);
+        }
+
+        public void RemoveTracks(string author)
+        {
+            foreach (Track track in Tracks)
+            {
+                if (track.Author == author)
+                {
+                    Tracks.Remove(track);
+                    break;
+                }
+            }
+        }
+
+        public void RemoveTrack(string title)
+        {
+            foreach (Track track in Tracks)
+            {
+                if (track.Title == title)
+                {
+                    Tracks.Remove(track);
+                    break;
+                }
+            }
+        }
+
+        public void RemoveTracks(params string[] titles)
+        {
+            foreach (Track track in Tracks)
+            {
+                if (titles.Any(x => x == track.Title))
+                {
+                    Tracks.Remove(track);
+                }
+            }
+        }
+    }
+}
---
+++ Eventovnia.Admin.Common/Track.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventovnia.Admin.Common
+{
+    public class Track
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public TimeSpan Length { get; set; }
+        public TimeSpan StartMusic { get; set; }
+        public TimeSpan EndMusic { get; set; }
+        public Uri Path { get; set; }
+
+        public Track() { }
+
+        public Track(Uri path)
+        {
+            Path = path;
+        }
+
+        public Track(string author, string title, TimeSpan length, Uri path)
+        {
+            Author = author;
+            Title = title;
+            Length = length;
+            Path = path;
+            StartMusic = new TimeSpan(0, 0, 0, 0);
+            EndMusic = Length;
+        }
+
+        public Track(string author, string title, TimeSpan length, Uri path, TimeSpan startMusic, TimeSpan endMusic)
+        {
+            Author = author;
+            Title = title;
+            Length = length;
+            Path = path;
+            StartMusic = startMusic;
+            EndMusic = endMusic;
+        }
+    }
+}
