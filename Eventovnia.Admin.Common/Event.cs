dd9a25f9e0499f629914fabf0f25a675612713bf
Krzysztof Klekot <krzysztof.klekot@atos.net>
2018-11-07 12:55:40 +0100

    Add clase Event

---
+++ Eventovnia.Admin.Common/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventovnia.Admin.Common
+{
+    class Event
+    {
+        public string Name { get; set; }
+        public int NumberOfPlaylists { get; set; }
+        public Event()
+        {
+
+        }
+
+    }
+}
