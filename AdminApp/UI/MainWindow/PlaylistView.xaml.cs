0166d113c120939bd03f66ffdff09c58eb60abc1
Krzysztof Klekot <krzysztof.klekot@atos.net>
2018-11-07 12:49:43 +0100

    Add class Event

--- AdminApp/UI/MainWindow/PlaylistView.xaml.cs
+++ AdminApp/UI/MainWindow/PlaylistView.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AdminApp.MainWindow
+namespace AdminApp.UI.MainWindow
 {
     /// <summary>
-    /// Interaction logic for PlaylistUI.xaml
+    /// Interaction logic for PlaylistViewUI.xaml
     /// </summary>
-    public partial class PlaylistUI : UserControl
+    public partial class PlaylistViewUI : Window
     {
-        public PlaylistUI()
+        public PlaylistViewUI()
         {
             InitializeComponent();
         }
-        
     }
 }
