38f25c8a92dc76d0b6e8a701ba680f2f4a5c5b4d
Krzysztof Klekot <krzysztof.klekot@atos.net>
2019-02-19 09:10:14 +0100

    a

--- Eventovnia.Common/Properties/AssemblyInfo.cs
+++ Eventovnia.Common/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Eventovnia.Admin.Common")]
+[assembly: AssemblyTitle("Eventovnia.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Eventovnia.Admin.Common")]
+[assembly: AssemblyProduct("Eventovnia.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
